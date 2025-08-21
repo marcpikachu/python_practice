4fc522aeabcf94a11a8f03f1e85416a4719551f1
marcpikachu <forcervp0206@gmail.com>
2025-08-22 00:18:51 +0800

    feat:day2 practice

---
+++ day2.py
@@ -0,0 +1,52 @@
+# Day 2 – FizzBuzz 練習
+# 請寫一個程式，印出 1 到 20 的數字。
+# 如果數字能被 3 整除，印出 "Fizz"
+# 如果數字能被 5 整除，印出 "Buzz"
+# 如果同時能被 3 和 5 整除，印出 "FizzBuzz"
+# 否則印出數字本身
+
+# range(start, end) 的規則
+# start → 起始值（包含）
+# end → 結束值（不包含）
+
+
+
+for i in range(1, 21):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
+
+# 判斷奇數偶數
+
+for i in range(1, 11):
+    if i % 2 == 0:
+        print(i, "是偶數")
+    else:
+        print(i, "是奇數")
+
+# 九九乘法表
+
+for i in range(1, 10):      # 1 到 9
+    for j in range(1, 10):  # 1 到 9
+        print(i, "x", j, "=", i*j)
+    print("---")  # 分隔線
+
+# 計算字串長度
+
+text = "Hello Python"
+print("字串長度:", len(text))    # 12
+
+# 找出字串裡有幾個母音 (a, e, i, o, u)
+
+text = "I am learning Python"
+count = 0
+for char in text.lower():
+    if char in "aeiou":
+        count += 1
+print("母音數量:", count)
+
